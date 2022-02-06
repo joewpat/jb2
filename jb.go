497424ef821a430e6d54417a3a38ad85341cb796
Joe Patrick <joewpat@gmail.com>
2022-02-06 16:05:33 -0500

    catching up

---
+++ jb.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"time"
+)
+
+func jb(query string) string {
+	url := "https://joe.surf/jb/raw/" + `"` + query + `"`
+	client := &http.Client{Timeout: 5 * time.Second}
+	req, _ := http.NewRequest("GET", url, nil)
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println(err)
+		return ""
+	}
+	defer resp.Body.Close()
+
+	responseData, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println(err)
+		return ""
+	}
+	return fmt.Sprintf("%s", responseData)
+}
